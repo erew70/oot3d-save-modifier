4aaefe40d8189c89536c114a6d8f24ec90440569
jon <97558900+erew70@users.noreply.github.com>
2024-09-14 12:56:14 -0500

    Update data_offsets.py

--- tool/util/data/data_offsets.py
+++ tool/util/data/data_offsets.py
@@ -3,8 +3,8 @@ skip_opening_cutscene_bytes_1 = bytes.fromhex("BB 00 00 00 01 00 00 00 00 00 00 
 skip_opening_cutscene_bytes_2 = bytes.fromhex("00 00 30 00 30 00 00 00 00 00 00 00 1A 00 00 00")
 skip_opening_cutscene_bytes_3 = bytes.fromhex("09 00 00 00 0A 00 00 00 10 00 00 00 02 00 00 00")
 
-skip_opening_offsets_1 = 0x00
-skip_opening_offsets_2 = 0x00
+skip_opening_offsets_1 = 0x0
+skip_opening_offsets_2 = 0x10
 
 skip_opening_offsets_2 = 0x40
 skip_opening_offsets_3 = 0x50
